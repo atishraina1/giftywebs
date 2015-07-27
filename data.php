f34e55cfb4895da93f53705186c408751f6c73d7
Atish Raina <atish.raina@giftywebs.in>
2015-07-28 01:06:22 +0530

    Changes added

--- data.php
+++ data.php
@@ -1,10 +1,17 @@
+<html>
+
 <?php
 
 $from="no-reply@giftywebs.in";
 $email="atish.raina91@gmail.com";
-$name=$_POST['Name'];
-mail ( $email, $name, "From:".$from);
-
-Print "Message Sent";
+$name=$_POST['inputName'];
+$emil=$_POST['inputEmail'];
+$query=$_POST['inputMessage'];
+$temp = $emil . $query;
+mail($email, $name, $temp, "From:".$from);
 
 ?>
+
+Your query has been submitted. Kindly <a href="about.html" style = "font-size: 20; font-weight: bold"> Click Here </a> to go back.
+
+</html>
